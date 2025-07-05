e6914500d665fd2c014f1c052afd1111635b661c
fighter4 <31556129+fighter4@users.noreply.github.com>
2025-07-04 22:05:11 -0400

    Delete src directory

--- src/app/auth/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthForm } from '@/components/app/auth-form';
-
-export default function AuthPage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen p-4">
-      <AuthForm />
-    </div>
-  );
-}
